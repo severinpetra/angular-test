0e591a30acf985ff18beee2943651bacee62177a
severinpetra <severinpetra@gmail.com>
2015-07-28 22:50:45 +0400

    1.Фильтрация и сортировка

--- app/scripts/controllers/SimpleList.js
+++ app/scripts/controllers/SimpleList.js
@@ -8,6 +8,13 @@ angular.module( 'myApp' ).controller( 'SimpleList', SimpleList );
 
 function SimpleList( $scope ) {
 
-    $scope.name = SimpleList.name;
+    $scope.label1 = 'Поиск:';
+    $scope.label2 = 'Всего:';
+    $scope.label3 = 'искомый символ';
+    $scope.label4 = 'Сортировка: ';
+
+    $scope.list = [3245,6785,43534,896,23452,4530];
+
+    $scope.order = 'null';
 
 }
